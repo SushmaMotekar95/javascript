5970524e99e13f0cac33a17b47b1dc6fef614ffa
SushmaMotekar95 <sushamajun95@gmail.com>
2023-05-11 16:55:07 +0530

    use of typeof
    using type of to find type of datatype

---
+++ 03_typeOf.js
@@ -0,0 +1,28 @@
+console.log("--Before Initialization (Variable value and type)--");
+var bankName;
+console.log("Bank Name:=>",bankName,"\nType:=>",typeof(bankName),"\n");
+
+console.log("--After Initialization (Variable value and type)--");
+var bankName="SBI Bank";
+console.log("Bank Name:=>",bankName," \nType:=>",typeof(bankName),"\n");
+
+console.log("--Marks (Variable value and type)--");
+var marks="90%";
+console.log("Marks=>",marks,"\nType:=>",typeof(marks),"\n");
+
+console.log("--Before Initialization (Variable value and type)--");
+var isWorking;
+console.log("isWorking:=>",isWorking,"\nType:=>",typeof(isWorking),"\n");
+
+console.log("--After Initialization (Variable value and type)--");
+var isWorking="true";
+console.log("isWorking:=>",isWorking,"\nType:=>",typeof(isWorking),"\n");
+
+console.log("--Total Count (Variable value and type)--");
+var totalCount="Hundred and Seven";
+console.log("Total Count:=>",totalCount,"\nType:=>",typeof(totalCount),"\n");
+
+console.log("--Update Total Count (Variable value and type)--");
+var totalCount=107;
+console.log("Total Count:=>",totalCount,"\nType:=>",typeof(totalCount),"\n");
+
