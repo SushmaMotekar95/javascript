06e79511c1cd075a7c0ad87111bafea4b10351e2
SushmaMotekar95 <sushamajun95@gmail.com>
2023-05-11 16:48:04 +0530

    swap variable
    swap two name and number

---
+++ 02__swapVariables.js
@@ -0,0 +1,25 @@
+console.log("/***********After Swap Name************");
+var name1="Sweety"
+var name2="Cutie"
+console.log("1st Name:=>",name1,"\n2nd Name:=>",name2);
+
+console.log("\n/***********Before Swap Name************");
+var temp
+temp=name1
+name1=name2
+name2=temp
+console.log("1st Name:=>",name1,"\n2nd Name:=>",name2);
+
+console.log("\n/***********After Swap Number************");
+num1=100
+num2=200
+num3=300
+console.log("1st Number:=>",num1,"\n2nd Number:=>",num2,"\n3rd Number:=>",num3);
+
+console.log("\n/***********Before Swap Number************");
+var temp
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+console.log("1st Number:=>",num1,"\n2nd Number:=>",num2,"\n3rd Number:=>",num3);
