cf5c707a7f1db5af6b7f99f724dbc80747927ac3
SushmaMotekar95 <sushamajun95@gmail.com>
2023-05-11 16:53:38 +0530

    assign variable
    assign Variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,34 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName="Sushama Motekar";
+console.log("My Full Name:=>"+myFullName);
+
+myFullName="Sushama Pralhad Motekar";
+console.log("My Full Name(First Middle Last name:=>)"+myFullName);
+
+var myLove="I Love only JS";
+console.log("favourite language:=>"+myLove);
+
+var firstName="Sushama";
+console.log("First Name:=>"+firstName);
+
+var lastName="Motekar";
+console.log("Last Name:=>"+lastName);
+
+var age="26";
+console.log("Age:=>"+age);
+
+var address="At Post Dhamodi tal. Raver Dist. Jalgoan";
+console.log("Address:=>"+address);
+
+var pincode="425507";
+console.log("Pincode:=>"+pincode);
+
+var gender="Female";
+console.log("Gender:=>"+gender);
+
+var IsMarried="NO";
+console.log("Is Married:=>"+IsMarried);
+
+
