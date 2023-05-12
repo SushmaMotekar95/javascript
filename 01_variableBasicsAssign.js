bec37c88a976abdfa06973a8dbc60a3e2afecf9d
SushmaMotekar95 <sushamajun95@gmail.com>
2023-05-12 13:39:00 +0530

    assign variable
    variable initialization and undefined ,null use

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,27 @@
+console.log("/*******Number********/");
+var  seatNo=343407;
+console.log("Seat-No:=>",seatNo,"\nType:=>",typeof(seatNo),"\n");
+
+console.log("/*******String********/");
+var fullName="Sushama Pralhad Motekar";
+console.log("Full-Name:=>",fullName,"\nType:=>",typeof(fullName),"\n");
+
+console.log("/*******Boolean*******/");
+var knowAngular="true";
+console.log("iS Angular Developer:=>",knowAngular,"\nType:=>",typeof(knowAngular),"\n");
+
+console.log("/******Difference between undefined and null*******");
+console.log("Undefined:=>");
+console.log("- undefined means variable is declared but It's value is not aasigned.");
+console.log("- typeof undefined as undefined.\n");
+console.log("Null:=>");
+console.log("- null means variable is declared but It's value is aasigned as null value.");
+console.log("- typeof null as object.\n");
+
+console.log("/**Example of undefined and null data type**/");
+var fatherName;
+console.log("Father Name is:=>",fatherName,"\nType:=>",typeof(fatherName),"\n");
+var motherName=null;
+console.log("Mother Name is:=>",motherName,"\nType:=>",typeof(motherName),"\n");
+
+
